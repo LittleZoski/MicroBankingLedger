e9f1f1b03207dec2734349a378c3cd793927f28a
Littlezoski <shicheng.zhou@my.utsa.edu>
2022-10-14 12:37:36 -0500

    selection 4 now return customer created with ID

--- src/main/java/service/CustomerCreation.java
+++ src/main/java/service/CustomerCreation.java
@@ -1,5 +1,7 @@
 package service;
 import Model.Customer;
+
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 import java.util.Scanner;
 
@@ -11,8 +13,8 @@ public class CustomerCreation {
         Scanner customerCreation = new Scanner(System.in);
 
         Connection conn = null;
-        //ResultSet rs;
-        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        PreparedStatement pstmt = null;
         //Connor's String url = "jdbc:sqlite:C:\\Users\\bta91388\\IdeaProjects\\bankingproject\\MicroBankLedger.db";
         //Frank's String url = "jdbc:sqlite:C:\\Users\\31243\\OneDrive\\Desktop\\Code\\MCC bootcamp formal\\bankingproject\\MicroBankLedger.db";
         String url = "jdbc:sqlite:C:\\Users\\31243\\OneDrive\\Desktop\\Code\\MCC bootcamp formal\\bankingproject\\MicroBankLedger.db";
@@ -37,17 +39,20 @@ public class CustomerCreation {
             Class.forName("org.sqlite.JDBC");
             conn = DriverManager.getConnection(url);
             //since customer id is autoIncrement we dont declare it here.
-            stmt = conn.prepareStatement("Insert into Customer (Name, DOB, PhoneNumber, StreetAddress, City, State)" +
-                    "VALUES(?, ?, ?, ?, ?, ?)" );
-            stmt.setString(1, customer.getName());
-            stmt.setDate(2, customer.getDOB());
-            stmt.setString(3, customer.getPhoneNumber());
-            stmt.setString(4, customer.getStreetAddress());
-            stmt.setString(5, customer.getCity());
-            stmt.setString(6, customer.getState());
+            pstmt = conn.prepareStatement("Insert into Customer (Name, DOB, PhoneNumber, StreetAddress, City, State)" +
+                    "VALUES(?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, customer.getName());
+            pstmt.setDate(2, customer.getDOB());
+            pstmt.setString(3, customer.getPhoneNumber());
+            pstmt.setString(4, customer.getStreetAddress());
+            pstmt.setString(5, customer.getCity());
+            pstmt.setString(6, customer.getState());
+            pstmt.executeUpdate();
             //need to add ZipCode
-            stmt.execute();
 
+            //to output the last index we have to use generateKeys method return the primary key auto generated to rs
+            rs = pstmt.getGeneratedKeys();
+            System.out.println("Customer Created with ID "+rs.getInt(1));
 
         } catch (Exception e){
             e.printStackTrace();
